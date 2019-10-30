c56d5658e07bbb267c300d1b857b61eac662529e
merikbest <merikbest2015@gmail.com>
2019-10-30 22:10:41 +0300

    queue

---
+++ src/Collections/c31/task3103/test.java
@@ -0,0 +1,52 @@
+package Collections.c31.task3103;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+//        Files.walkFileTree(Paths.get("C:\\prog\\SolutionTest"), new MyFileVisitor());
+//        MyFileVisitor myFileVisitor = new MyFileVisitor();
+//        myFileVisitor.setName("test1");
+        Path directory  = Paths.get("C:\\prog\\SolutionTest");
+        String fileNameToFind = "test1";
+
+        try {
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes fileAttributes) {
+                    if (path.toFile().getName().contains(fileNameToFind)) {
+                        System.out.println("Found: " + path.toString());
+                        return FileVisitResult.TERMINATE;
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class MyFileVisitor extends SimpleFileVisitor<Path> {
+    String fileNameToFind = "test1";
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (file.toFile().getName().equals(fileNameToFind)) {
+            System.out.println("Found: " + file.toString());
+            return FileVisitResult.TERMINATE;
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
