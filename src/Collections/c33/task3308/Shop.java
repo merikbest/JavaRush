b5ba1c00d7357a594c13d0c9aea097214eac0b69
merikbest <merikbest2015@gmail.com>
2019-11-17 15:49:54 +0300

    done

---
+++ src/Collections/c33/task3308/Shop.java
@@ -0,0 +1,22 @@
+package Collections.c33.task3308;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlType(name = "shop")
+@XmlRootElement
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    public String[] secretData;
+
+    public static class Goods {
+        @XmlElementWrapper(name = "goods", nillable = true)
+        List<String> names = new ArrayList<>();
+    }
+}
