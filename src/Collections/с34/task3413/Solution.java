808e8b3e2e8c1cec4b16f17983e1b2b48a81cddb
merikbest <merikbest2015@gmail.com>
2019-11-18 18:13:04 +0300

    done

---
+++ src/Collections/с34/task3413/Solution.java
@@ -0,0 +1,34 @@
+package Collections.с34.task3413;
+
+/*
+Кеш на основании SoftReference
+Реализуй логику методов get, put, remove класса SoftCache:
+
+Метод AnyObject get(Long key) должен возвращать объект типа AnyObject из мапы cacheMap по ключу key. Если такого ключа в cacheMap нет - верни null.
+
+Метод AnyObject put(Long key, AnyObject value) должен добавлять в мапу пару key : value. Метод должен вернуть null, если в cacheMap по такому ключу ранее не было значения. Иначе - верни предыдущее значение value по этому ключу. Не забудь вызвать метод clear() у объекта типа SoftReference<AnyObject>.
+
+Метод AnyObject remove(Long key) должен удалить из мапы cacheMap пару key : value по ключу key. Метод должен вернуть null, если в cacheMap по такому ключу ранее не было значения. Иначе - верни предыдущее значение value по этому ключу. Не забудь вызвать метод clear() у объекта типа SoftReference<AnyObject>.
+
+Не изменяй класс AnyObject.
+Метод main не принимает участия в тестировании.
+
+Требования:
+•	Класс AnyObject не должен быть изменен.
+•	В классе SoftCache должно существовать приватное поле Map<Long, SoftReference<AnyObject>> cacheMap.
+•	Реализуй метод get согласно условию.
+•	Реализуй метод put согласно условию.
+•	Реализуй метод remove согласно условию.
+*/
+
+import sun.misc.SoftCache;
+
+public class Solution {
+    public static void main(String[] args) {
+        SoftCache cache = new SoftCache();
+
+        for (long i = 0; i < 2_500_000; i++) {
+            cache.put(i, new AnyObject(i, null, null));
+        }
+    }
+}
