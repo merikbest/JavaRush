5735b6d72c3ddf84fe12d6ab79146d7631eab650
merikbest <merikbest2015@gmail.com>
2019-12-04 22:41:42 +0300

    done

---
+++ src/Collections/c36/task3601/View.java
@@ -0,0 +1,8 @@
+package Collections.c36.task3601;
+
+public class View  {
+    private Controller controller = new Controller();
+    public void fireShowDataEvent() {
+        System.out.println(controller.onShowDataList());
+    }
+}
